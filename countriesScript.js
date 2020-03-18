d16fc5aa3551a63b8b04fd6d894711ce06d00baa
Zangana <rahimra@dukes.jmu.edu>
2020-03-18 23:05:34 +0300

    added a search box and changed the link of the API

--- countriesScript.js
+++ countriesScript.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest()
-request.open('GET', 'https://corona.lmao.ninja/countries')
+request.open('GET', 'https://coronavirus-19-api.herokuapp.com/countries')
 request.onload = function(){
     var data = JSON.parse(this.response)
     allCases = data;
@@ -12,34 +12,52 @@ request.onload = function(){
     var container = document.getElementById("row-container");
     var content="";
     data.forEach(function(result,i){
+        if(result == [] || result == null){
+            container.innerHTML = '<p>چاوەروانی ئاماری تازە... تکایە دوای کەمێکی تر سەردانمان بکەوە</p>'
+        }
         if(i == 0){
             content += '<div class="row">'
+        }
+        if(i!=0 && i%5 ==0){
+            content += '</div><div class="row">'
         }
         content += 
             '<div class="col">'+
                 '<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">'+
-                    '<div class="card-header font-weight-bold text-center">'+result.country.toUpperCase()+'</div>'+
+                    '<div class="card-header font-weight-bold text-center" id='+result.country.toUpperCase()+'>'+result.country.toUpperCase()+'</div>'+
                         '<div class="card-body">'
-                            +'<h5 class="card-title border text-right">ژ.تووشبوو: '+result.cases+'</h5>'
-                            +'<p class="card-text border text-right">'
+                            +'<h4 class="card-title border px-2 text-right">ژ.تووشبوو: '+result.cases+'</h4>'
+                            +'<h4 class="card-text border px-2 text-right">'
                             +'ژ.گیان لە دەستدان: '+result.deaths 
                             +'<br>'
                             +'ژ.چاک بوو: '+result.recovered
                             +'<br>'
-                            +'ژ.تووشبووی امرۆ: '+result.todayCases
+                            +'ژ.تووشبووی ئەمرۆ: '+result.todayCases
                             +'<br>'
-                            +'ژ.گ.امرۆ: '+result.todayDeaths
-                            +'</p>'
+                            +'ژ.گ.ئەمرۆ: '+result.todayDeaths
+                            +'</h4>'
                         +'</div>'
                 +'</div>'
             +'</div>'
-        if(i!=0 && i%4 ==0){
-            content += '</div><div class="row">'
-        }
     });
     content += '</div>'
     
     container.innerHTML += content;
-
+    
 }
 request.send()
+function searchFun(){
+    var searchValue = document.getElementById("searchBox").value.toUpperCase();
+    var search = document.getElementById("searchBox");
+    search.addEventListener("keyup",function(event){
+        if (event.keyCode === 13){
+            event.preventDefault();
+            document.getElementById("btnSearch").click();
+        }
+    })
+   // alert(document.getElementById(search));
+   document.getElementById(searchValue).scrollIntoView({
+       behavior: 'smooth'
+
+   });
+}
--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest()
-request.open('GET', 'https://corona.lmao.ninja/all')
+request.open('GET', 'https://coronavirus-19-api.herokuapp.com/all')
 var allCases = null
 request.onload = function(){
     var data = JSON.parse(this.response)
@@ -9,15 +9,17 @@ request.onload = function(){
     }else{
         console.log('error')
     }
+    var args_string = JSON.stringify(data)
+    var all = document.getElementById("all");
     var numberCases = document.getElementById("numberCases");
     var numberRecovery = document.getElementById("numberRecovery");
     var numberDeath = document.getElementById("numberDeath");
     var getCases = data.cases;
     var getRecovery = data.recovered;
     var getDeath = data.deaths;
-
     numberCases.append(getCases)
     numberRecovery.append(getRecovery)
     numberDeath.append(getDeath)
+   // all.append(args_string);
 }
 request.send()
